473ddf0002fb8c6957d9b61b396e3e88b450261b
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-07-11 17:45:17 +0100

    feat: updated default greeting text

--- src/chat/greeting.ts
+++ src/chat/greeting.ts
@@ -14,7 +14,7 @@ const greeting = () => async (ctx: Context) => {
 	const userName = `${ctx.message?.from.first_name} ${ctx.message?.from.last_name}`;
 
 	if (messageId) {
-		await replyToMessage(ctx, messageId, `Hello, ${userName}!`);
+		await replyToMessage(ctx, messageId, `Hi, ${userName}!`);
 	}
 };
 
